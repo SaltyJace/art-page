2c8c216deadc6f6c68390bf06787ca8b9f041368
Jace <107281078+SaltyJace@users.noreply.github.com>
2023-05-17 14:11:40 +0000

    changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 window.onload = function() {
   var gallery = document.getElementById("gallery");
-  var numImages = 5;
+  var numImages = 9;
   var images = [];
   for (var i = 1; i <= numImages; i++) {
     images.push("img" + i + ".jpg");
