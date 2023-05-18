aec5b2c1b592187e1c672f8ac13ce735ed8b1cb3
Jace <107281078+SaltyJace@users.noreply.github.com>
2023-05-18 18:29:54 +0000

    lightbox in progress

--- script.js
+++ script.js
@@ -1,13 +1,20 @@
-window.onload = function() {
-  var gallery = document.getElementById("gallery");
-  var numImages = 9;
-  var images = [];
-  for (var i = 1; i <= numImages; i++) {
-    images.push("img" + i + ".jpg");
-  }
-  for (var i = 0; i < images.length; i++) {
-    var img = document.createElement("img");
-    img.src = "img/" + images[i];
-    gallery.appendChild(img);
-  }
-};
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
